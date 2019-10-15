72d0a57c501c07043bdcc6f805c693b6b766c7dd
Almas Sharipkhan <almassharipkhan@gmail.com>
2019-10-16 02:26:02 +0600

    Lab3 final

--- CSLab2/Controllers/CalculatorController.cs
+++ CSLab2/Controllers/CalculatorController.cs
@@ -6,24 +6,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CSLab2.Controllers
 {
+    //public class CalculatorController : Controller
+    //{
+    //    //public IActionResult PlusTen(int number)
+    //    //{
+    //    //    ViewData["number"] = number;
+    //    //    ViewData["result"] = number + 10;
+
+    //    //    return View();
+    //    //}
+
+    //    [Route("[controller]/{firstNumber:int}/{secondNumber:int}")]
+    //    public IActionResult Sum(int firstNumber, int secondNumber)
+    //    {
+    //        ViewData["firstNumber"] = firstNumber;
+    //        ViewData["secondNumber"] = secondNumber;
+    //        ViewData["result"] = firstNumber + secondNumber;
+
+    //        return View();
+    //    }
+    //}
+
+    [Route("[controller]/[action]")]
     public class CalculatorController : Controller
     {
-        //public IActionResult PlusTen(int number)
-        //{
-        //    ViewData["number"] = number;
-        //    ViewData["result"] = number + 10;
-
-        //    return View();
-        //}
-        
-        [Route("[controller]/{firstNumber:int}/{secondNumber:int}")]
+        [Route("{firstNumber:int}/{secondNumber:int}")]
         public IActionResult Sum(int firstNumber, int secondNumber)
         {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '+';
             ViewData["firstNumber"] = firstNumber;
             ViewData["secondNumber"] = secondNumber;
             ViewData["result"] = firstNumber + secondNumber;
 
-            return View();
+            return View("Result");
+        }
+
+        [Route("{firstNumber:int}/{secondNumber:int}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
         }
     }
 }
