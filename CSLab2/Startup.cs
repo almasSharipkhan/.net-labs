a43d2e3e0d08a44ae67a31dd347575db2c692817
Almas Sharipkhan <almassharipkhan@gmail.com>
2019-10-16 01:35:26 +0600

    Final commit

--- CSLab2/Startup.cs
+++ CSLab2/Startup.cs
@@ -15,6 +15,7 @@ namespace CSLab2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +25,15 @@ namespace CSLab2
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Hello}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles();
         }
     }
 }
