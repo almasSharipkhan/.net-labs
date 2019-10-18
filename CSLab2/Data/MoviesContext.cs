6a2c2d85b015251577c699c28c941d6f09dc2796
Almas Sharipkhan <almassharipkhan@gmail.com>
2019-10-18 23:26:03 +0600

    Lab5

---
+++ CSLab2/Data/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using CSLab2.Models.Movies;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSLab2.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- CSLab2/Startup.cs
+++ CSLab2/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CSLab2.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSLab2
 {
@@ -15,6 +17,10 @@ namespace CSLab2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MoviesContext>(options =>
+            {
+                options.UseSqlite("Filename=movies.db");
+            });
             services.AddMvc();
         }
 
